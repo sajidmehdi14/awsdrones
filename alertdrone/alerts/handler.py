471b52bc6eea0fecf2920b27524fb877d939faaf
sajidmehdi14 <sajidmehdi14@gmail.com.com>
2021-01-25 14:53:22 +0500

    Adding Lambda function with serverless and deployment cicd pielines

--- alertdrone/alerts/handler.py
+++ alertdrone/alerts/handler.py
@@ -2,6 +2,7 @@ import json
 
 
 def hello(event, context):
+    print(event)
     return {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
--- alertdrone/alerts/scale-up.py
+++ alertdrone/alerts/scale-up.py
@@ -3,7 +3,4 @@ session = boto3.Session()
 import boto3
 session = boto3.Session(profile_name='default')
 as_client = session.client('autoscaling')
-as_client.describe_auto_scaling_groups()
-as_client.describe_policies()
 as_client.execute_policy(AutoScalingGroupName='alertdrone-asg', PolicyName='scale up')
-as_client.execute_policy(AutoScalingGroupName='alertdrone-asg', PolicyName='scale down')
