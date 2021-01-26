2562d2fb6782dda721a02791d7acea8c316c3d70
sajidmehdi14 <sajidmehdi14@gmail.com.com>
2021-01-26 16:00:34 +0500

    Send Alerts to a slack's channel when AutoScaling group scales up and its Cloudwatch event is generated.

--- alertdrone/alerts/handler.py
+++ alertdrone/alerts/handler.py
@@ -1,7 +1,13 @@
 import os
 
+import requests
+
 def post_to_slack(event, context):
     slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
-    print(event)
+
+    slack_message = "From {source} at {detail[StartTime]}: {detail[Description]}".format(**event)
+
+    data = { "text": slack_message }
+    requests.post(slack_webhook_url, json=data)
 
     return
